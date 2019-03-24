729a20748f85eede0ce19f556bae11581da7ed1a
Massimo Tseng <massimo.tseng@gmail.com>
2019-03-23 23:56:29 -0700

    animephae support added

--- js/script.js
+++ js/script.js
@@ -15,12 +15,21 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 })
 const getTitle = (url) =>{
     let title
-    if (url.indexOf("9anime.to\/watch") > -1) {
+    if (url.indexOf('9anime.to\/watch') > -1) {
         title = $('h1.title')[0].innerHTML;
         let episodes = document.querySelector('a.active').innerHTML;
         if (episodes.charAt(0) == 0) episodes = episodes.substring(1, episodes.length);
         title = title + episodes
         setInfo(title, 3);
+    }
+    else if(url.indexOf('animepahe.com/play/')){
+        title = $('h1 a')[0].title
+        episodes =  $('h1')[0].textContent
+        episodes = episodes.substring(8 + title.length, episodes.length - 6)
+        episodes = episodes.trim()
+        title = `${title} Ep.${episodes}`
+        setInfo(title, 3)
+
     }
 }
 const setInfo = (title, gameType) => {
